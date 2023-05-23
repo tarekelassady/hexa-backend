93c89a37165ec14c7b6995fc74d0b2cc9fedeefd
tarekelassadym <tarekelassady@gmail.com>
2023-05-23 15:33:00 +0200

    2

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use((err,req,res,next)=>{
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './upload')
+    cb(null, 'https://hexa-backend.onrender.com/upload')
   },
   filename: (req, file, cb) => {
     cb(null, Date.now()+file.originalname.replace(/\s+/g, '-').toLowerCase());
